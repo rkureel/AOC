c93b2116a4020bc0681a76e20c6e62f9a6e0db20
Rahul <r.dev2000@gmail.com>
2021-12-16 16:49:46 +0530

    Added solutions for Day 6

---
+++ 2021/day_6/day6_puzzle2.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func get_input(f *os.File) map[int]int {
+	scanner := bufio.NewScanner(f)
+	m := make(map[int]int)
+	for scanner.Scan() {
+		line := scanner.Text()
+		numbers := strings.Split(line, ",")
+		for _, number := range numbers {
+			number_int, _ := strconv.Atoi(number)
+			m[number_int] += 1
+		}
+	}
+	return m
+}
+
+
+func main() {
+	f, _ := os.Open("input.txt")
+	m := get_input(f)
+	var (
+		i int
+		j int
+	)
+	dp := make([][]int, 9)
+	for i=0;i<9;i++ {
+		dp[i] = make([]int, 257)
+	}
+	
+	dp[0][1] = 2
+	for i=1;i<9;i++ {
+		dp[i][1] = 1
+	}
+	for i=2;i<257;i++ {
+		for j=0;j<9;j++ {
+			if j==0 {
+				dp[j][i] = dp[6][i-1] + dp[8][i-1]
+			} else {
+				dp[j][i] = dp[j-1][i-1]
+			}
+		}
+	}
+	sum := 0
+	for i=0;i<9;i++ {
+		sum += m[i]*dp[i][256]
+	}
+	fmt.Println(sum)
+}
