bcace0c203b219e98d67c3ab597342df1b07a1e1
Rahul <r.dev2000@gmail.com>
2021-12-23 04:42:59 +0530

    Added solutions for Day 16

---
+++ 2021/day_16/day16_puzzle1.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func get_input(f *os.File) string {
+	scanner := bufio.NewScanner(f)
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func hex_to_binary(hex string) string {
+	mapper := map[rune]string{
+		'0': "0000",
+		'1': "0001",
+		'2': "0010",
+		'3': "0011",
+		'4': "0100",
+		'5': "0101",
+		'6': "0110",
+		'7': "0111",
+		'8': "1000",
+		'9': "1001",
+		'A': "1010",
+		'B': "1011",
+		'C': "1100",
+		'D': "1101",
+		'E': "1110",
+		'F': "1111",
+	}
+
+	var bits strings.Builder
+	for _, char := range hex {
+		bits.WriteString(mapper[char])
+	}
+	return bits.String()
+}
+
+var packet string
+
+func parse_packet(start int, end int) (int64, int64, int) {
+	version_number, _ := strconv.ParseInt(packet[start:start+3], 2, 32)
+	type_id, _ := strconv.ParseInt(packet[start+3:start+6], 2, 32)
+	if type_id == 4 {
+		var literal_value strings.Builder
+		curr := 6+start
+		for {
+			var last bool = false
+			if packet[curr] == '0' {
+				last = true
+			}
+			literal_value.WriteString(packet[curr+1:curr+5])
+			curr += 5
+			if last {
+				break
+			}			
+		}
+		literal_number, _ := strconv.ParseInt(literal_value.String(), 2, 32)
+		return version_number, literal_number, curr
+	} else {
+		if packet[start+6] == '0' {
+			len_packet, _ := strconv.ParseInt(packet[start+7:start+7+15], 2, 32)
+			last_bit := len_packet+7+15+int64(start)
+			packet_end := start+7+15
+			for {
+				packet_version, _, next_bit := parse_packet(packet_end, int(last_bit))
+				packet_end = next_bit
+				version_number += packet_version
+				if packet_end >= int(last_bit) {
+					break
+				}
+			}
+			return version_number, 0, int(last_bit)
+		} else {
+			n_packets, _ := strconv.ParseInt(packet[start+7:start+7+11], 2, 32)
+			packet_end := start+7+11
+			for ;n_packets>0;n_packets--{
+				packet_version, _, next_bit := parse_packet(packet_end, end)
+				packet_end = next_bit
+				version_number += packet_version
+			}
+			return version_number, 0, packet_end
+		}
+	}
+}
+
+func main() {
+	f, _ := os.Open("input.txt")
+	hex_input := get_input(f)
+	packet = hex_to_binary(hex_input)
+	
+	version_number, _, _ := parse_packet(0, len(packet))
+	fmt.Println(version_number)
+}
